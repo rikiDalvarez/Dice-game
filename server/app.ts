824f4c27a50b17fef8dc68db9671fd25e8ff549a
Galis <galis.travelling@gmail.com>
2023-08-02 13:39:27 +0200

    testing merge 2

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,12 @@
-// import express from "express";
+import express from "express";
 
-// const app = express();
-// const port = 5555;
+const app = express();
+const port = 5555;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-// app.listen(port, () => {
-//   console.log(`Server is listening on port ${port}! 🍄 `);
-// });
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}! 🍄 `);
+});
