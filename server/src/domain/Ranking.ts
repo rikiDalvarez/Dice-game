b82f166bca9a6c481de118cbbabe0b2fc27d8106
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-08-07 11:01:13 +0200

    Ranking test

--- server/src/domain/Ranking.ts
+++ server/src/domain/Ranking.ts
@@ -3,8 +3,10 @@ import { PlayerDetailsType } from "./PlayerList";
 
 export class Ranking {
   readonly ranking: Array<PlayerDetailsType>;
+  private _looser:Array<Player>;
   constructor(players: Array<Player>) {
     this.ranking = this.preparePlayersDetails(players);
+    this._looser = []
   }
 
   private preparePlayersDetails(players: Array<Player>): Array<PlayerDetailsType> {
@@ -12,7 +14,6 @@ export class Ranking {
       return { name: player.name, rating: player.successRate };
     });
   }
-
   
 
 }
