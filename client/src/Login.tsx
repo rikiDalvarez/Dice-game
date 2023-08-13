f2e433bce7dd78b7e7943c977cb1b239e6dbb2d0
Ricardo <ric.dalvarez@hotmail.com>
2023-08-13 03:06:45 +0200

    add background effect

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -42,7 +42,7 @@ const Login: React.FC = () => {
 
 
 	return (
-		<div className="min-h-screen flex items-center justify-center bg-gray-100 ">
+		<div className="min-h-screen flex items-center justify-center bg-color-movement ">
 			<div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
 				<h2 className="text-2xl font-semibold mb-4">Login</h2>
 				<form onSubmit={handleSubmit}>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+      },
+      keyframes: {
+        wave: {
+          '0%': { backgroundColor: 'green' },
+          '10%': { backgroundColor: 'blue' },
+          '20%': { backgroundColor: 'purple' },
+          '30%': { backgroundColor: 'red' },
+          '40%': { backgroundColor: 'orange' },
+          '50%': { backgroundColor: 'yellow' },
+          '60%': { backgroundColor: 'white' },
+          '100%': { backgroundColor: 'dark' },
+        },
+      },
+      animation: {
+        'changinColor': 'wave 2s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
