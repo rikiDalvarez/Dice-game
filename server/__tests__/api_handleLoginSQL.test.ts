11e5e33bdab45143e95960245bb77a5e219390de
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-08-13 16:56:06 +0200

    loginHandler tests for SQL

---
+++ server/__tests__/api_handleLoginSQL.test.ts
@@ -0,0 +1,52 @@
+import supertest from "supertest";
+import { server } from "../src/Server";
+// import bcrypt from "bcrypt";
+import { app } from "../src/app";
+import { describe, afterAll, beforeEach } from "@jest/globals";
+import { createUser } from "../auxilaryFunctionsForTests/createUser";
+import { PlayerSQL } from "../src/infrastructure/models/mySQLModels/PlayerMySQLModel";
+import { GameSQL } from "../src/infrastructure/models/mySQLModels/GameMySQLModel";
+import { sequelize } from "../src/infrastructure/mySQLConnection";
+const api = supertest(app);
+
+describe("API POST PLAYER TEST", () => {
+  beforeEach(async () => {
+    await PlayerSQL.destroy({
+      where: {}
+    })
+    await GameSQL.destroy({
+      where: {}
+    })
+    await createUser(api, "first password", "first.anonim@op.pl");
+  });
+
+  it("Should login player", async () => {
+    await api
+      .post("/api/login")
+      .send({ password: "first password", email: "first.anonim@op.pl" })
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  it("should have an authorization error with wrong password", async () => {
+    await api
+      .post("/api/login")
+      .send({ password: "test", email: "first.anonim@op.pl" })
+      .expect(401)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  it("should have an authorization error with wrong email", async () => {
+    await api
+      .post("/api/login")
+      .send({ password: "first password", email: "test.anonim@op.pl" })
+      .expect(401)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+    server.close();
+    
+  });
+});
