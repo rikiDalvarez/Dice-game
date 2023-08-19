736c4aa5e8cea07f2f17c902709d3e987da69f91
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-08-19 10:02:58 +0200

    mend

--- server/__tests__/auxilaryFunctionsForTests/testStart.ts
+++
@@ -1,7 +0,0 @@
-import config from "../../config/config";
-import { start } from "../../src/app";
-
-const databaseName = config.NODE_ENV==="test"? config.TEST_DATABASE: config.DATABASE
-
-
-start(config.DATABASE_ENV, databaseName )
