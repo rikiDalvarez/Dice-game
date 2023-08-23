89362867d84a97ef363311a136b8d745bf5b4080
Ricardo <ric.dalvarez@hotmail.com>
2023-08-23 17:02:39 +0200

    add registration

--- client/src/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Navbar({ isAuthenticated }) {
-	return (
-		<div>Navbar</div>
-	)
-}
-
-export default Navbar
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+
+function Navbar() {
+	return (
+		<div>Welcome user</div>
+	)
+}
+
+export default Navbar
