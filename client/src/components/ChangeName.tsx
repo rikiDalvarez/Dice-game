677bad4c772c196fe2e9f6f4da82e7ab66ad7de1
Ricardo <ric.dalvarez@hotmail.com>
2023-08-26 16:43:42 +0200

    fix bits style

--- client/src/components/ChangeName.tsx
+++ client/src/components/ChangeName.tsx
@@ -1,10 +1,10 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { changeName } from "../services";
 
 //   router.put("/players/:id", auth, playerRootControllers.changeName);
 
 export type ChangeNameProps = {
-  stateChanger: (param:boolean) => void
+  stateChanger: (param: boolean) => void
 };
 
 export const ChangeName: React.FC<ChangeNameProps> = (props) => {
@@ -18,46 +18,47 @@ export const ChangeName: React.FC<ChangeNameProps> = (props) => {
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
-		try {
-			const response = await changeName(token,player_id, inputField);
-			if (response.ok) {
-				const data = await response.json();
-				if (data.name === inputField){
-                    console.log('Name changed succesfully')
-                }else{
-                console.error("Error ocurred during changing name")}
-			} else {
-				console.error(response)
-			}
-		} catch (error) {
-			console.error("an error occurred:", error)
-		}
-        props.stateChanger(false)
+    try {
+      const response = await changeName(token, player_id, inputField);
+      if (response.ok) {
+        const data = await response.json();
+        if (data.name === inputField) {
+          console.log('Name changed succesfully')
+        } else {
+          console.error("Error ocurred during changing name")
+        }
+      } else {
+        console.error(response)
+      }
+    } catch (error) {
+      console.error("an error occurred:", error)
+    }
+    props.stateChanger(false)
   };
 
   return (
-    <section className="min-h-screen flex items-center justify-center bg-color-movement">
-      <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
-        <form className="form" onSubmit={handleSubmit}>
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email"
-          >
-            Introduce new name
-          </label>
-          <input
-            className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
-            type="text"
-            id="newName"
-            onChange={(e) => handleInputChange(e)}
-            required
-          />
 
-          <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300" 
+    <div className=" w-full p-6 bg-white rounded-lg shadow-lg">
+      <form className="form" onSubmit={handleSubmit}>
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Introduce new name
+        </label>
+        <input
+          className="w-full p-2 border rounded-md focus:outline-none focus:border-blue-500"
+          type="text"
+          id="newName"
+          onChange={(e) => handleInputChange(e)}
+          required
+        />
+
+        <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"
           type="submit">Submit</button>
-        </form>
-      </div>
-    </section>
+      </form>
+    </div>
+
   );
 };
 
--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -1,1 +1,1 @@
-export const PORT = "5000";
+export const PORT = "8000";
