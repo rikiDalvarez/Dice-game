a5372f060a811ee1c4c202c073c0fdbddaf5b30d
Galis <galis.travelling@gmail.com>
2023-08-27 15:36:22 +0200

    adding message to Delete Games

--- client/src/components/UserDataManipulation.tsx
+++ client/src/components/UserDataManipulation.tsx
@@ -1,30 +1,34 @@
-import React, {useEffect,  useState } from "react";
+import React, { useEffect, useState } from "react";
 import ChangeName from "./ChangeName";
 import PlayGame from "./PlayGame";
 import DeleteGames from "./DeleteGames";
 
-type UserDataManipulation = {
-  dashboardStateChanger: (state: string) => void
-}
+type UserDataManipulationProps = {
+  dashboardStateChanger: (state: string) => void;
+};
 
-const UserDataManipulation: React.FC <UserDataManipulation> = (props) => {
+const UserDataManipulation: React.FC<UserDataManipulationProps> = (props) => {
   const [changeName, setChangeName] = useState(false);
   const [isGameInProgress, setGameInProgress] = useState(false);
+  const [gamesDeleted, setGamesDeleted] = useState(false);
 
-  useEffect(()=>{
-    if (isGameInProgress){
-      props.dashboardStateChanger('played')
+  useEffect(() => {
+    if (isGameInProgress) {
+      props.dashboardStateChanger("played");
     }
-    if (changeName){
-      props.dashboardStateChanger('nameChanged')
+    if (changeName) {
+      props.dashboardStateChanger("nameChanged");
     }
+  }, [changeName, isGameInProgress, props]);
 
-  }, [changeName, isGameInProgress, props])
- 
   return (
     <div className="userDataManipulation border-2 border-sky-500 m-4 p-4 flex flex-col rounded-lg">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-       onClick={()=>{setGameInProgress(true)}}
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={() => {
+          setGameInProgress(true);
+          setGamesDeleted(false);
+        }}
       >
         Play Game
       </button>
@@ -32,18 +36,26 @@ const UserDataManipulation: React.FC <UserDataManipulation> = (props) => {
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick={() => {
           setChangeName(true);
+          setGamesDeleted(false);
         }}
       >
         Change Name
       </button>
-      {changeName ? (
-        <ChangeName stateChanger={setChangeName} />
-      ) : (
-        ""
+      {changeName ? <ChangeName stateChanger={setChangeName} /> : ""}
+      <PlayGame
+        newGame={isGameInProgress}
+        playGameChanger={setGameInProgress}
+      />
+      <DeleteGames
+        onGamesDeleted={() => {
+          setGamesDeleted(true);
+        }}
+      />
+      {gamesDeleted && (
+        <div className="bg-green-300 text-green-900 p-2 mt-2">
+          All games were successfully deleted.
+        </div>
       )}
-      <PlayGame newGame={isGameInProgress} playGameChanger={setGameInProgress}/>
-      {<DeleteGames />}
-      {/* <ChangeName /> */}
     </div>
   );
 };
