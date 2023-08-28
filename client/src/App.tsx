df82601f68b4dc2daf9119e6b8224cef67705b5e
Ricardo <ric.dalvarez@hotmail.com>
2023-08-28 15:05:02 +0200

    fix import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState,useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import { fetchToken } from './services';
