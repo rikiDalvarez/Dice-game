5ae01ded2852e56cb46f2a6ce65f511612b37122
Ricardo <ric.dalvarez@hotmail.com>
2023-08-29 01:42:30 +0200

    fix average title

--- client/src/components/RankingList.tsx
+++ client/src/components/RankingList.tsx
@@ -6,7 +6,7 @@ function RankingList({ ranking }) {
 		<>
 			{ranking.ranking.length > 0 ? (
 				<div className='bg-blue-200 rounded-lg m-4 p-4 max-h-96 overflow-y-auto'>
-					<div className="average ">average : {ranking.average}</div>
+					<div className="average text-lg font-extrabold ">average : {ranking.average}</div>
 					{ranking.ranking.map((player) => (
 						<div className="card font-mono w-96" key={player.name}>
 							<div className={`m-2 p-2 border-2 ${(player.successRate > 19)
