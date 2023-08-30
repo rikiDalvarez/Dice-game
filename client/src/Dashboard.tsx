96f07857deb11663b716fedb10a7810387b5a50b
Konrad Winnicki <konrad.winnicki@protonmail.com>
2023-08-30 21:35:51 +0200

    getRanking refactor

--- client/src/Dashboard.tsx
+++ client/src/Dashboard.tsx
@@ -1,140 +1,148 @@
-import React, { useState, useEffect, useContext } from 'react';
-import Navbar from './components/Navbar';
-import Login from './Login';
-import UserDataManipulation from './components/UserDataManipulation';
-import PlayerList from './components/PlayerList';
-import GetGameData from './components/GetGameData';
-import GameList from './components/GameList';
-import { useNavigate } from 'react-router-dom';
-import { fetchToken } from './services';
-import { IPlayer } from './components/Player';
-import RankingList from "./components/RankingList"
-import { UserContext } from './context/UserContext';
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "./components/Navbar";
+import Login from "./Login";
+import UserDataManipulation from "./components/UserDataManipulation";
+import PlayerList from "./components/PlayerList";
+import GetGameData from "./components/GetGameData";
+import GameList from "./components/GameList";
+import { useNavigate } from "react-router-dom";
+import { fetchToken } from "./services";
+import { IPlayer } from "./components/Player";
+import RankingList from "./components/RankingList";
+import { UserContext } from "./context/UserContext";
 import jwt_decode from "jwt-decode";
-
+import { JwtPayload } from "jwt-decode";
 
 //after refreshing the dashboard we lose userContext
 
 export interface playerRanking {
-	name: string | null;
-	successRate: number;
+  name: string | null;
+  successRate: number;
 }
 export interface IRanking {
-	ranking: playerRanking[];
-	average: number;
-
+  ranking: playerRanking[];
+  average: number;
 }
 
 interface DashboardProps {
-	name?: string | null;
-	id?: string | null;
+  name?: string | null;
+  id?: string | null;
 }
 
-
 const Dashboard: React.FC<DashboardProps> = ({ name, id }) => {
+  const [data, setData] = useState<Array<IPlayer> | null>(null);
+  const [refreshGameList, setRefreshGameList] = useState(false);
+  //const [ranking, setRanking] = useState<IRanking[] | null>(null);
+  const [isRankingShown, showRanking] = useState(false);
 
-	const [data, setData] = useState<Array<IPlayer> | null>(null);
-	const [refreshGameList, setRefreshGameList] = useState(false);
-	const [ranking, setRanking] = useState<IRanking | null>(null)
+  const userContext = useContext(UserContext);
 
-	const userContext = useContext(UserContext);
+  //using context instead of localStorage
+  const { user } = userContext;
+  console.log("userContextDashboard", user);
 
-	//using context instead of localStorage
-	const { user } = userContext;
-	console.log("userContextDashboard", user)
+  const handleRankingSetUp = (data: IRanking[]) => {
+    setRanking(data);
+  };
 
-	const handleRankingSetUp = (data: IRanking[]) => {
-		setRanking(data)
+  const handleRefreshGames = () => {
+    setRefreshGameList(true);
+  };
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    setData(null);
+    navigate("/");
+  };
+
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decodedToken: JwtPayload = jwt_decode(token);
+    const currentDate = new Date();
+    const tokenExpiration = decodedToken.exp ? decodedToken.exp : null;
+    if (tokenExpiration) {
+      if (tokenExpiration * 1000 < currentDate.getTime()) {
+        console.log("Token expired.");
+      } else {
+        console.log("Valid token");
+      }
+    }
+  }
+
+  useEffect(() => {
+    const fetchProtectedData = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          const response = await fetchToken(token);
+
+          if (response.ok) {
+            const responseData = await response.json();
+
+            setData(responseData.playerList.reverse());
+          } else {
+            console.error("Fetching players");
+          }
+        } else {
+          console.error("Token not found");
+        }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+    if (refreshGameList) {
+      fetchProtectedData();
+      setRefreshGameList(false);
+    }
+    if (!data) {
+      fetchProtectedData();
+    }
+
+	if (data){
+		showRanking(false)
 	}
+  }, [refreshGameList, data]);
 
-	const handleRefreshGames = () => {
-		setRefreshGameList(true);
-	};
+  return (
+    <div className="flex-col">
+      {data ? (
+        <>
+          <Navbar name={name} />
+          <div className="m-5  border-t-4 border-double border-emerald-950 flex ">
+            <UserDataManipulation handleRefreshGames={handleRefreshGames} />
 
-	const navigate = useNavigate();
-
-	const logout = () => {
-		localStorage.clear()
-		setData(null)
-		navigate("/")
-	}
-
-	const token = localStorage.getItem("token");
-	if (token) {
-		const decodedToken = jwt_decode(token)
-		console.log("decodedTOKEN", decodedToken)
-		const currentDate = new Date();
-		if (decodedToken.exp * 1000 < currentDate.getTime()) {
-			console.log("Token expired.");
-		} else {
-			console.log("Valid token");
-		}
-	}
-
-
-	useEffect(() => {
-		const fetchProtectedData = async () => {
-			try {
-				const token = localStorage.getItem('token');
-
-				if (token) {
-					const response = await fetchToken(token)
-
-					if (response.ok) {
-						const responseData = await response.json();
-
-						setData(responseData.playerList.reverse());
-
-					} else {
-						console.error('Fetching players');
-					}
-				} else {
-					console.error('Token not found');
-				}
-			} catch (error) {
-				console.error('An error occurred:', error);
-			}
-		};
-		if (refreshGameList) {
-			fetchProtectedData();
-			setRefreshGameList(false)
-		}
-		if (!data) {
-			fetchProtectedData();
-		}
-
-
-	}, [refreshGameList, data]);
-
-
-
-	return (
-		<div className='flex-col'>
-			{data ? (
-				<>
-					<Navbar name={name} />
-					<div className="m-5  border-t-4 border-double border-emerald-950 flex ">
-						<UserDataManipulation
-							handleRefreshGames={handleRefreshGames}
-						/>{ranking
-							? (
-								<RankingList ranking={ranking} />
-							) : (
-								<PlayerList props={data} />
-							)}
-						<GetGameData handleRankingSetUp={handleRankingSetUp} handleRefreshGames={handleRefreshGames} setData={setData} />
-					</div>
-					<GameList id={id} refreshGames={refreshGameList} />
-					<div>
-						<button onClick={logout} className="bg-amber-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
-					</div>
-				</>
-			) : (
-				<Login />
-			)}
-		</div>
-	);
-
+            
+            {isRankingShown ? (
+              <RankingList
+                setRankingInProgress={showRanking}
+              />
+            ) : (
+				<PlayerList props={data} />
+            )}
+            <GetGameData
+              showRanking={showRanking}
+              handleRefreshGames={handleRefreshGames}
+              setData={setData}
+            />
+          </div>
+          <GameList id={id} refreshGames={refreshGameList} />
+          <div>
+            <button
+              onClick={logout}
+              className="bg-amber-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+          </div>
+        </>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
--- client/src/components/RankingList.tsx
+++ client/src/components/RankingList.tsx
@@ -1,36 +1,67 @@
-import React from 'react'
-import { IPlayer } from './Player'
+import { IPlayer } from "./Player";
+import { fetchGetRanking } from "../services";
+import React, { useEffect, useState } from "react";
 
-function RankingList({ ranking }) {
 
-	return (
-		<>
-			{ranking.ranking.length > 0 ? (
-				<div className='bg-blue-200 rounded-lg m-4 p-4 max-h-96 overflow-y-auto'>
-					<div className="average text-lg font-extrabold ">average : {ranking.average}</div>
-					
-					
-					{ranking.ranking.map((player:IPlayer) =>{
-						const playerName = player.name? player.name: "Anonim"
-						return (
-						<div className="card font-mono w-96" key={player.id}>
-							<div className={`m-2 p-2 border-2 ${(player.successRate > 19)
-								? 'bg-green-200'
-								: player.successRate > 10
-									? 'bg-amber-300'
-									: 'bg-red-200'}`} >
-								<h1>name: {playerName}</h1>
-								<p>average: {player.successRate}</p>
-							</div>
-						</div>
-					)})}
-				</div>
-			) : (
-				"blaaa"
-			)}
-		</>
-	)
 
+export interface RankingListI {
+  ranking: Array<IPlayer>;
+  average: number;
 }
 
-export default RankingList
+const RankingList: React.FC = () => {
+  const [rankingList, setRankingList] = useState([]);
+  const [average, setAverage] = useState();
+
+  const getRankingList = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetchGetRanking(token);
+      if (response.ok) {
+        const responseData = await response.json();
+        setRankingList(responseData.ranking);
+        //setAverage(responseData.average);
+        console.log(responseData);
+      } else {
+        console.error("fetching games");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+  useEffect(() => {
+    getRankingList();
+  }, []);
+
+  return (
+    <>
+      <div className="bg-blue-200 rounded-lg m-4 p-4 max-h-96 overflow-y-auto">
+        <div className="average text-lg font-extrabold ">
+          average : {average}
+        </div>
+        {rankingList.map((player: IPlayer) => {
+          const playerName = player.name ? player.name : "Anonim";
+          return (
+            <div className="card font-mono w-96" key={player.id}>
+              <div
+                className={`m-2 p-2 border-2 ${
+                  player.successRate > 19
+                    ? "bg-green-200"
+                    : player.successRate > 10
+                    ? "bg-amber-300"
+                    : "bg-red-200"
+                }`}
+              >
+                <h1>name: {playerName}</h1>
+                <p>average: {player.successRate}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default RankingList;
